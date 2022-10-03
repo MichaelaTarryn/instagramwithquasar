a3f5c4dd5bd7e849487fdc4e8c7265ad0ecdbc77
MichaelaTarryn <mward7006@gmail.com>
2022-10-03 14:52:27 +0200

    first

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+    class="bg-white text-grey-10"
+    bordered
+    >
+      <q-toolbar>
+        <q-toolbar-title
+        class="text-grand-hotel text-bold"
+
+         >
+          QuasarGram
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-footer
+    class="bg-white"
+    bordered>
+          <q-tabs
+          class="text-grey-10"
+          active-color="primary"
+          indicator-color="transparent"
+      >
+        <q-route-tab  icon="eva-home-outline"
+         to="/"
+         />
+        <q-route-tab
+         icon="eva-camera-outline"
+        to="/camera" />
+      </q-tabs>
+      </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default ({
+  name: 'MainLayout',
+  components: {
+
+  },
+
+})
+</script>
+<style lang="sass">
+.q-toolbar__title
+    text-align: center
+.q-footer
+  .q-tab_icon
+    font-size: 30px
+</style>
---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+   <h5>
+    CameraPage
+   </h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageCamera'
+})
+</script>
